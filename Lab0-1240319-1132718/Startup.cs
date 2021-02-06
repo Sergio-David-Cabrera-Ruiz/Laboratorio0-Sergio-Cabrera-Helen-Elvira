a9e5ee18953af6e57a640fa96762db2a8986b816
SergioCabrera <sergiodcr0258@gmail.com>
2021-02-05 18:42:50 -0600

    Creación del almacenamiento y controlador y vista de cliente

--- Lab0-1240319-1132718/Startup.cs
+++ Lab0-1240319-1132718/Startup.cs
@@ -26,7 +26,7 @@ namespace Lab0_1240319_1132718
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<ContextoNegocio>(options => options.UseSqlServer(Configuration.GetConnectionString("ConeccionDefault")));
+            services.AddDbContext<ContextoNegocio>(options => options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
             services.AddControllersWithViews();
         }
 
